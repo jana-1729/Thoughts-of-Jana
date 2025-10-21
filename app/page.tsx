9909d23bc20c495082acda91a6485007e1c37bb2
Janarthanan S <janarthanan.s@surveysparrow.com>
2025-10-21 08:47:53 +0530

    Lint Fix

--- app/page.tsx
+++ app/page.tsx
@@ -203,6 +203,44 @@ export default function Home() {
                     <i className="bx bx-right-arrow-alt hidden group-hover:visible"></i>
                   </div>
                 </a>
+                <a href="https://medium.com/@janarthanans.in/server-sent-events-real-time-data-streaming-made-simple-8fbe8301018b">
+                  <div
+                    className="text-blue-700 font-medium text-md md:text-lg py-2
+                  hover:px-1
+                  transition-all duration-200
+                  flex justify-between items-center
+                  group
+                  hover:text-purple-700
+                  "
+                  >
+                    <div className="flex items-center justify-center gap-6">
+                      <span className="h-1 w-1 rounded bg-gray-600 inline-flex group-hover:rounded-none group-hover:bg-purple-500"></span>
+                      <p className="group-hover:text-red">
+                      Server-Sent Events : Real-Time Data Streaming Made Simple
+                      </p>
+                    </div>
+                    <i className="bx bx-right-arrow-alt hidden group-hover:visible"></i>
+                  </div>
+                </a>
+                <a href="https://medium.com/@janarthanans.in/oauth-2-0-delegated-authorization-f89f3c759c95">
+                  <div
+                    className="text-blue-700 font-medium text-md md:text-lg py-2
+                  hover:px-1
+                  transition-all duration-200
+                  flex justify-between items-center
+                  group
+                  hover:text-purple-700
+                  "
+                  >
+                    <div className="flex items-center justify-center gap-6">
+                      <span className="h-1 w-1 rounded bg-gray-600 inline-flex group-hover:rounded-none group-hover:bg-purple-500"></span>
+                      <p className="group-hover:text-red">
+                      OAuth 2.0: Delegated Authorization
+                      </p>
+                    </div>
+                    <i className="bx bx-right-arrow-alt hidden group-hover:visible"></i>
+                  </div>
+                </a>
               </div>
             </div>
           </div>
